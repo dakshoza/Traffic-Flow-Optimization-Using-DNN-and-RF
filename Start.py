9fe5c7f4431fabcf952fd313f011a4daa6c8cec0
Scott-Reilly <scott.reilly30@gmail.com>
2023-05-15 23:01:56 +0530

    Basic background

--- Start.py
+++ Start.py
@@ -1,2 +1,18 @@
 import pygame
-pygame.init()   #initialise
+pygame.init()
+window = pygame.display.set_mode((960,540))
+
+# class car:
+#     def __init__(self,spawnLocation):
+
+running = True
+background = pygame.image.load('BG1.png')
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    window.fill((81,81,81))
+    window.blit(background,(0,0))
+
+
+    pygame.display.update()
